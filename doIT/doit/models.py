e67ccad139fa6dff92ca7b5c320254b2297ec016
kaganndemirr <o.kagan.demir.28@gmail.com>
2017-07-11 15:45:20 +0300

    Add files via upload
    K.ad=Doit, Sifre=admin2017 models.py de reminders users tasks eklendi

--- doIT/doit/models.py
+++ doIT/doit/models.py
@@ -1,11 +1,7 @@
 from django.db import models
+from datetime import datetime
 
-class User(models.Model):
-    e_mail=models.EmailField(max_length=200, blank=True, verbose_name="E-Posta")
-    first_name=models.CharField(max_length=200, blank=True, verbose_name="Ad")
-    last_name=models.CharField(max_length=200, blank=True, verbose_name="Soyad")
-    password=models.CharField(max_length=50, blank=True, verbose_name="Şifre")
-
-class Tasks(models.Model):
-    title=models.CharField(max_length=1000, blank=True, verbose_name="Hatırlatma")
-    user=models.ForeignKey(User, verbose_name="Kullanıcı")
+class Reminders(models.Model):
+	task = models.ForeignKey(Tasks,verbose_name="Görev")
+	time = datetime.datetime.now().strftime('%H:%M:%S')
+	date = datetime.now()
