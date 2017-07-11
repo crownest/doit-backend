93fdd24cf8ffdf81786d4c64945c28405da739c0
DurmusYASAR <durmusyasar13@gmail.com>
2017-07-11 15:33:39 +0300

    K.ad=Doit, Sifre=admin2017 , models.py'de User ve Tasks olusturuldu admin panelinde gösterildi

--- doIT/doit/admin.py
+++ doIT/doit/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from doit.models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Tasks)
--- doIT/doit/models.py
+++ doIT/doit/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    e_mail=models.EmailField(max_length=200, blank=True, verbose_name="E-Posta")
+    first_name=models.CharField(max_length=200, blank=True, verbose_name="Ad")
+    last_name=models.CharField(max_length=200, blank=True, verbose_name="Soyad")
+    password=models.CharField(max_length=50, blank=True, verbose_name="Şifre")
+
+class Tasks(models.Model):
+    title=models.CharField(max_length=1000, blank=True, verbose_name="Hatırlatma")
+    user=models.ForeignKey(User, verbose_name="Kullanıcı")
