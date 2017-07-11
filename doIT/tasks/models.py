5247cf61c4379cafbb0440f01b799ac94fcd12c3
DurmusYASAR <durmusyasar13@gmail.com>
2017-07-11 17:53:05 +0300

    modeller ayrıldı , pep8 uyumlu yapıldı

--- doIT/tasks/models.py
+++ doIT/tasks/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 class Task(models.Model):
-        title = models.CharField(max_length=1000, blank=True, verbose_name="Reminding")
-
+        title = models.CharField(max_length=1000, blank=True,
+                                 verbose_name="Reminding")
 
         class Meta:
             ordering = ['title']
@@ -11,9 +11,11 @@ class Task(models.Model):
         def __str__(self):
             return self.title
 
+
 class Reminder(models.Model):
         task = models.ForeignKey(Task, verbose_name="Task")
-        creation_date = models.DateTimeField(default=timezone.now, verbose_name="Time")
+        creation_date = models.DateTimeField(default=timezone.now,
+                                             verbose_name="Time")
 
         def __str__(self):
             return u'%s, %s,' % (self.task, self.creation_date)
