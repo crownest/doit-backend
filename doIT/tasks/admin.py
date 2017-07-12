096c17141c200defd6a550455e7de489b0233012
kaganndemirr <o.kagan.demir.28@gmail.com>
2017-07-12 15:49:44 +0300

    açıklama extenden descriptionda
    task paneli düzenlendi remindersa title filtresi eklendi kod pep 8 uyumlu hale getirildi

--- doIT/tasks/admin.py
+++ doIT/tasks/admin.py
@@ -5,7 +5,7 @@ from tasks.models import Reminder, Task
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     fields = ('title', 'description')
-    list_display = ('title','description')
+    list_display = ('title', 'description')
     list_filter = ('title',)
     search_fields = ('description', 'title',)
 
@@ -17,11 +17,12 @@ class TaskAdmin(admin.ModelAdmin):
         return obj.description
     get_desc.admin_order_field = 'description'
 
+
 @admin.register(Reminder)
 class ReminderAdmin(admin.ModelAdmin):
     fields = ('task', 'date',)
-    list_display = ('get_taskr_title', 'date', )
-    list_filter = ('date','task__title')
+    list_display = ('get_taskr_title', 'date')
+    list_filter = ('date', 'task__title')
     search_fields = ('task__title',)
 
     def get_taskr_title(self, obj):
