aaa98dc40a12b094392d01a4288fa3a7db0f3549
DurmusYASAR <durmusyasar13@gmail.com>
2017-07-12 11:59:12 +0300

    models.py düzenlendi

--- doIT/tasks/models.py
+++ doIT/tasks/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Task(models.Model):
-        title = models.CharField(max_length=1000, blank=True,
-                                 verbose_name="Reminding")
+        title = models.CharField(
+            verbose_name="Reminding", max_length=1000, blank=True
+        )
 
         class Meta:
-            ordering = ['title']
+            ordering = ('title',)
 
         def __str__(self):
-            return self.title
+            return '{title}'.format(title=self.title)
 
 
 class Reminder(models.Model):
-        task = models.ForeignKey(Task, verbose_name="Task")
-        creation_date = models.DateTimeField(default=timezone.now,
-                                             verbose_name="Time")
+        task = models.ForeignKey(
+            verbose_name='Task', to='tasks.Task', related_name='reminders'
+        )
+        date = models.DateTimeField(verbose_name='Time')
 
         def __str__(self):
-            return u'%s, %s,' % (self.task, self.creation_date)
+            return '{task} {date}'.format(task=self.task.__str__(), date=self.date)
 
         class Meta:
-            ordering = ['creation_date']
+            ordering = ('date',)
--- doIT/tasks/views.py
+++ doIT/tasks/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Task, Reminder
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+
+# def task_list(request):
+#     title = request.GET['title']
+#     tasks = Task.objects.filter(end_time__gte=timezone.localtime(
+#                                 timezone.now())).order_by('date')
+#     return HttpResponse("Your task : %s", % title)
+#     # return render(request, 'tasks_list.html', {'tasks':tasks})
+#
+#
+# def task_detail(request, pk):
+#     task = get_object_or_404(Task, pk=pk)
+#     return render(request, 'task_detail.html', {'task': task})
