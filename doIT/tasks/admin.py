844a90b2abd1595843856de3a383c1e475b83783
DurmusYASAR <durmusyasar13@gmail.com>
2017-07-12 14:46:46 +0300

    admin.py hazırlandı

--- doIT/tasks/admin.py
+++ doIT/tasks/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from tasks.models import Reminder, Task
 
-admin.site.register(Task)
-admin.site.register(Reminder)
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    fields = ('title', 'description')
+    list_display = ('title', 'get_task_title',)
+    list_filter = ('title',)
+    search_fields = ('description', 'title',)
+
+    def get_task_title(self, obj):
+        return obj.title
+    get_task_title.admin_order_field = 'title'
+    get_task_title.short_description = 'Tasks'
+
+@admin.register(Reminder)
+class ReminderAdmin(admin.ModelAdmin):
+    fields = ('task__title', 'date')
+    list_display = ('get_date_title', 'date', )
+    list_filter = ('date',)
+    search_fields = ('task__title',)
+
+    def get_date_title(self, obj):
+        return obj.task.title
+    get_date_title.admin_order_field = 'task'
+    get_date_title.short_description = 'Task'
--- doIT/tasks/models.py
+++ doIT/tasks/models.py
@@ -4,8 +4,12 @@ from django.utils import timezone
 
 class Task(models.Model):
         title = models.CharField(
-            verbose_name="Reminding", max_length=1000, blank=True
+            verbose_name='Reminder', max_length=1000, blank=True
         )
+        description = models.TextField(
+            verbose_name='Description', max_length=10000, null=True
+        )
+
 
         class Meta:
             ordering = ('title',)
@@ -18,10 +22,13 @@ class Reminder(models.Model):
         task = models.ForeignKey(
             verbose_name='Task', to='tasks.Task', related_name='reminders'
         )
-        date = models.DateTimeField(verbose_name='Time')
+        date = models.DateTimeField(verbose_name='Time', blank=True, null=True)
+
+        # def task_name(self):
+        #     return self.task.get(pk=self.pk)
 
         def __str__(self):
-            return '{task} {date}'.format(task=self.task.__str__(), date=self.date)
+            return '{task}'.format(task=self.task.__str__())
 
         class Meta:
             ordering = ('date',)
