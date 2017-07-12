2b8102aa85ab3ec6590b6ae7ada99ada324dd9d6
kaganndemirr <o.kagan.demir.28@gmail.com>
2017-07-12 15:46:20 +0300

    task paneli düzenlendi,reminders a title filtresi

--- doIT/tasks/admin.py
+++ doIT/tasks/admin.py
@@ -5,20 +5,23 @@ from tasks.models import Reminder, Task
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     fields = ('title', 'description')
-    list_display = ('title', 'get_task_title',)
+    list_display = ('title','description')
     list_filter = ('title',)
     search_fields = ('description', 'title',)
 
     def get_task_title(self, obj):
         return obj.title
     get_task_title.admin_order_field = 'title'
-    get_task_title.short_description = 'Tasks'
+
+    def get_desc(self, obj):
+        return obj.description
+    get_desc.admin_order_field = 'description'
 
 @admin.register(Reminder)
 class ReminderAdmin(admin.ModelAdmin):
     fields = ('task', 'date',)
     list_display = ('get_taskr_title', 'date', )
-    list_filter = ('date',)
+    list_filter = ('date','task__title')
     search_fields = ('task__title',)
 
     def get_taskr_title(self, obj):
