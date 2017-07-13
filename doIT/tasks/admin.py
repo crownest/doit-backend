28181b41114a13fb29d2f0409457c5d3eec58a64
kaganndemirr <o.kagan.demir.28@gmail.com>
2017-07-13 12:06:51 +0300

    tasks/admin.py de düzenleme

--- doIT/tasks/admin.py
+++ doIT/tasks/admin.py
@@ -4,26 +4,31 @@ from tasks.models import Reminder, Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    fields = ('title', 'description')
-    list_display = ('title', 'description')
+    fields = ('user','title','description')
+    list_display = ('get_task_title', 'get_desc')
     list_filter = ('title',)
-    search_fields = ('description', 'title',)
+    search_fields = ('title', 'description')
 
     def get_task_title(self, obj):
         return obj.title
+    get_task_title.short_description = "Reminder"
 
     def get_desc(self, obj):
         return obj.description
-    get_desc.admin_order_field = 'description'
+    get_desc.short_description = "Description"
 
 
 @admin.register(Reminder)
 class ReminderAdmin(admin.ModelAdmin):
     fields = ('task', 'date',)
-    list_display = ('get_taskr_title', 'date')
+    list_display = ('get_taskr_title', 'get_date')
     list_filter = ('date', 'task__title')
     search_fields = ('task__title',)
 
     def get_taskr_title(self, obj):
         return obj.task.title
-    get_taskr_title.short_description = 'Task'
+    get_taskr_title.short_description = "Tasks"
+
+    def get_date(self, obj):
+        return obj.date
+    get_date.short_description = "Date"
