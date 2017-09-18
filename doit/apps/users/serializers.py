508404bc2fc6317ec3509b4fa8ddf18284942e20
kenansubasi <kenansubasiceng@gmail.com>
2017-09-18 17:37:27 +0300

    refs #59: User Change Password service updated.

--- doit/apps/users/serializers.py
+++ doit/apps/users/serializers.py
@@ -46,3 +46,41 @@ class UserUpdateSerializerV1(UserSerializer):
         fields = (
             'email', 'first_name', 'last_name'
         )
+
+
+class UserPasswordChangeSerializer(serializers.Serializer):
+    old_password = serializers.CharField()
+    new_password = serializers.CharField()
+    confirm_new_password = serializers.CharField()
+
+    class Meta:
+        fields = (
+            'old_password', 'new_password', 'confirm_new_password'
+        )
+
+    def validate_confirm_new_password(self, value):
+        if self.initial_data['new_password'] != self.initial_data['confirm_new_password']:
+            raise serializers.ValidationError(
+                "The two password fields didn't match."
+            )
+
+        return value
+
+    def validate_old_password(self, value):
+        user = self.context['user']
+
+        if not user.check_password(value):
+            raise serializers.ValidationError(
+                'Your old password was entered incorrectly. '
+                'Please enter it again.'
+            )
+
+        return value
+
+
+class UserPasswordChangeSerializerV1(UserPasswordChangeSerializer):
+
+    class Meta:
+        fields = (
+            'old_password', 'new_password', 'confirm_new_password'
+        )
