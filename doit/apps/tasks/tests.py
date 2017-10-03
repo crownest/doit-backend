d52aabc48a34ba985b80e1e4d25a898fb8597d4d
kenansubasi <kenansubasiceng@gmail.com>
2017-10-03 17:36:48 +0300

    refs #76: Task retrieve test written.

--- doit/apps/tasks/tests.py
+++ doit/apps/tasks/tests.py
@@ -1,4 +1,35 @@
-# Django
-from django.test import TestCase
+# Third-Party
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+# Django
+from django.urls import reverse
+
+# Local Django
+from users.models import User
+from tasks.models import Task
+
+
+class TaskAPIV1TestCase(APITestCase):
+    task_dummy_data = {
+        'title': 'Sport',
+        'description': 'Basketball'
+    }
+
+    def setUp(self):
+        self.email = "doit@unicrow.com"
+        self.password = "test"
+        self.user = User.objects.create_user(self.email, self.password)
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_retrieve_task(self):
+        task = Task.objects.create(user=self.user, **self.task_dummy_data)
+        url = reverse('v1:tasks-detail', kwargs={'pk': task.id})
+
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
